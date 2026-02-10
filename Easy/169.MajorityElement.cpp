eb4592b1b6b3c22a858866e152503226ff2480a0
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-02-10 22:25:16 +0530

    169

--- Easy/169.MajorityElement.cpp
+++ Easy/169.MajorityElement.cpp
@@ -5,14 +5,14 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        for (int val : nums){
-            int freq = 0;
-            for ( int el : nums){
-            if(el == val){
-                freq ++;
+        for (int val : nums){ //For each element in the array nums 
+            int freq = 0; //Declare the variable to count the frequency
+            for ( int el : nums){ //This for loop is intiated to check the equivalence of the elements
+            if(el == val){ //This contional is applied to check the equivalence of the element with the value that was intiated in the first loop
+                freq ++; 
             }
         }
-        if(freq > n/2){
+        if(freq > n/2){ //If the freq is greater than half the size of the array then we return the value
             return val;
         }
     }
