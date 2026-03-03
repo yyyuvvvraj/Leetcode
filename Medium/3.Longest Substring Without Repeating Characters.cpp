1c8ead9b965c570138da3c1a931caba9d6f20656
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-03 17:34:41 +0530

    3

---
+++ Medium/3.Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256,-1);
+        int ans = 0, start = -1;
+        for(int i = 0 ; i < s.size(); i++){
+            if(dict[s[i]] > start){
+                start = dict[s[i]];
+            }
+            dict[s[i]] = i;
+            ans = max(ans,i-start);
+        }
+        return ans;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 03/03/26.
+//
