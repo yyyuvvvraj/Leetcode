64f4ca61f640daf4f9a9cdc639216950cd2e2e42
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-10 22:14:40 +0530

    42

---
+++ Hard/42. Trapping Rain Water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> lmax(n,0);
+        vector<int> rmax(n,0);
+        int ans = 0;
+        lmax[0] = height[0];
+        rmax[n-1] = height[n-1];
+        for(int i = 1; i < n; i++){
+            lmax[i] = max(lmax[i-1], height[i]);
+        }
+        for(int i = n - 2; i >= 0; i--){
+            rmax[i] = max(rmax[i + 1], height[i]);
+        }
+        for(int i = 0; i < n; i++){
+            ans += min(rmax[i],lmax[i]) - height[i];
+        }
+        return ans;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 09/03/26.
+//
