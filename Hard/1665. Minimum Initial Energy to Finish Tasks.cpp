733d6264e496972551424a2dcce283c422fbe334
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-05-12 18:29:22 +0530

    1665

---
+++ Hard/1665. Minimum Initial Energy to Finish Tasks.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    bool isPossible(vector<vector<int>>& tasks, int mid){
+        for(auto &task : tasks){
+            int actual = task[0];
+            int minimum = task[1];
+
+            if(minimum > mid){
+                return false;
+            }
+            mid -= actual;
+
+        }
+        return true;
+    }
+
+    int minimumEffort(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+
+        int l = 0;
+        int r = 1e9;
+
+        int result = INT_MAX;
+
+        auto lambda = [] (auto &task1, auto& task2){
+            int diff1 = task1[1] - task1[0];
+            int diff2 = task2[1] - task2[0];
+
+            return diff1 > diff2;
+        };
+
+        sort(begin(tasks), end(tasks), lambda);
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if(isPossible(tasks , mid)){
+                result = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return result;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 12/05/26.
+//
