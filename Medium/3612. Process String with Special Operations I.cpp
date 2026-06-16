2d80f776687dd40ddac89fde2c6603e8eb11e0a8
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-16 21:40:36 +0530

    3612

---
+++ Medium/3612. Process String with Special Operations I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+
+        for(char &ch : s){
+            if(ch == '*'){
+                if(result.length() > 0) result.pop_back();
+            }else if(ch == '#'){
+                result += result;
+            }else if(ch == '%'){
+                reverse(begin(result), end(result));
+            }else{
+                result += ch;
+            }
+        }
+        return result;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 16/06/26.
+//
