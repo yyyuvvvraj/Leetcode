2f9a4387a2266f6c5af14e8e43782548752c5e7e
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-17 22:31:52 +0530

    3614

---
+++ Hard/3614. Process String with Special Operations II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n = s.length();
+
+        long long L = 0;
+
+        for(char &ch : s) {
+            if(ch == '*') {
+                if(L > 0)
+                    L--;
+            } else if(ch == '#') {
+                L *= 2;
+            } else if(ch == '%') {
+                //reverse - no change in L
+                continue;
+            } else { //'a' to 'z'
+                L++;
+            }
+        }
+
+        if(k >= L) {
+            return '.';
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] == '*') {
+                L++; //no change in k
+            } else if(s[i] == '%') {
+                //no change in L
+                k = L - k - 1;
+            } else if(s[i] == '#') {
+                L = L/2;
+                k = (k >= L) ? k - L : k;
+            } else { //'a' to 'z'
+                L--;
+            }
+
+            if(k == L) {
+                return s[i];
+            }
+        }
+
+        return '.';
+
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 17/06/26.
+//
