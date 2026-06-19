9d8e28b7f15d43df48dfe30d5e1b4320d93e94f0
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-19 14:31:30 +0530

    1732

---
+++ Easy/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int prev = 0;
+        int max = 0;
+
+        for(auto x : gain){
+            prev += x;
+            if(prev > max){
+                max = prev;
+            }
+        }
+        return max;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 19/06/26.
+//
