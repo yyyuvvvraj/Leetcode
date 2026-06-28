feac92376ff887b3d3961e12bdb1735d2cae19b1
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-28 21:10:09 +0530

    1846

---
+++ Medium/1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+
+        sort(begin(arr), end(arr));
+
+        int maxEl = -1;
+
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                arr[0] = 1;
+            }else if(arr[i] - arr[i - 1] > 1){
+                arr[i] = arr[i - 1] + 1;
+            }
+
+            maxEl = max(maxEl, arr[i]);
+        }
+
+        return maxEl;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 28/06/26.
+//
