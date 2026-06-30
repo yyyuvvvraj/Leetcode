1b89c81e447b4836c2a4e3d37cbe28c7df27b629
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-06-30 20:02:55 +0530

    1967

---
+++ Medium/1358. Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        map<char,int>mp;
+        int i=0;
+        int j=0;
+        int n=s.size();
+        int ans=0;
+        while(i<n)
+        {
+            mp[s[i]]++;
+            if(mp.size()==3)
+            {
+                ans+=(n-i);
+                while(j<i)
+                {
+                    if(mp[s[j]]==1)
+                    {
+                        mp.erase(s[j]);
+                    }
+                    else{
+                        mp[s[j]]--;
+                    }
+                    j++;
+                    if(mp.size()==3)
+                    {
+                        ans+=(n-i);
+                    }
+                    else{
+                        break;
+                    }
+
+                }
+            }
+            i++;
+        }
+        while(j<i)
+        {
+            if(mp[s[j]]==1)
+            {
+                mp.erase(s[j]);
+            }
+            else{
+                mp[s[j]]--;
+            }
+            if(mp.size()==3)
+            {
+                ans+=(n-i);
+            }
+            else{
+                break;
+            }
+            j++;
+        }
+
+        return ans;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 30/06/26.
+//
