b92dcf8f8aa422c927a2c09bd39b69c62f6e969a
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-07-20 21:49:55 +0530

    1260

---
+++ Easy/1260. Shift 2D Grid.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int row = grid.size();
+        int col = grid[0].size();
+
+        int n = row * col;
+
+        k = k % n;
+
+        if(k == 0){
+            return grid;
+        }
+
+        auto reverse = [&](int i, int j){
+            while(i < j){
+                swap(grid[i / col][i % col], grid[j / col][j % col]);
+                i++;
+                j--;
+            }
+        };
+
+        reverse(0, n - 1);
+        reverse(0, k - 1);
+        reverse(k, n - 1);
+
+        return grid;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 20/07/26.
+//
