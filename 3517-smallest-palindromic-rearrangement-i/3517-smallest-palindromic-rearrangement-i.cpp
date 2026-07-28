d7250f80374cb62ae7bf25707098a434e3eb4835
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-07-28 21:55:35 +0530

    Time: 71 ms (42.86%), Space: 53.9 MB (98.46%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.length();
+
+        int mid = n / 2;
+
+        sort(begin(s),begin(s) + mid);
+
+        for(int i = 0; i < mid; i++){
+            s[n - 1 - i] = s[i];
+        }
+        return s;
+    }
+};
