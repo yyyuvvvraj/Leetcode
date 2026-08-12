451a6bf9c93427e6c3fe11ba57910168f6648e66
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-08-12 22:37:13 +0530

    2958

---
+++ Medium/2958. Length of Longest Subarray With at Most K Frequency.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        int i = 0;
+        int j = 0;
+        int result = 0;
+
+        while(j < n) {
+
+            mp[nums[j]]++;
+
+            while(i < j && mp[nums[j]] > k) {
+                mp[nums[i]]--;
+                i++;
+            }
+
+            result = max(result, j - i + 1);
+            j++;
+        }
+
+        return result;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 12/08/26.
+//
