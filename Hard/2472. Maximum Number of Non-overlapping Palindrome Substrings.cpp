a95b844c54bd6107dc34d7540c5e368bf7c465c3
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-09-15 21:44:41 +0530

    2472

---
+++ Hard/2472. Maximum Number of Non-overlapping Palindrome Substrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isPalindrome(const string& s, int i, int j) {
+        while (i < j) {
+            if (s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        if (k == 1)
+            return n; //each character can be a substring
+
+        vector<vector<int>> t(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= i; --j) {
+
+                if (isPalindrome(s, i, j)) {
+                    int growWindow  = t[i][j + 1];
+                    int takeIt      = 1 + (j + k <= n ? t[j + 1][j + k] : 0);
+                    int slideWindow = t[i + 1][j + 1];
+
+                    t[i][j] = max({growWindow, takeIt, slideWindow});
+                }
+
+                int slideWindow = t[i + 1][j + 1];
+                int growWindow  = t[i][j + 1];
+                t[i][j] = max({t[i][j], slideWindow, growWindow});
+            }
+        }
+
+        return t[0][k - 1];
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 15/09/26.
+//
