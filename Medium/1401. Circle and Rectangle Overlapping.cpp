ddfe7849d8480e77898ba851f01734ad493081e0
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-09-19 21:35:49 +0530

    1401

---
+++ Medium/1401. Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        //nearest point
+        int xi = clamp(xCenter, x1, x2);
+        int yi = clamp(yCenter, y1, y2);
+
+        if(y1 > yCenter) {
+            yi = y1;
+        } else if(y2 < yCenter) {
+            yi = y2;
+        } else {
+            yi = yCenter;
+        }
+
+        //(xi, yi) ------- (xCenter, yCenter)
+        return sqrt((xi - xCenter)*(xi - xCenter) + (yi - yCenter)*(yi - yCenter)) <= radius;
+    }
+};
+
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 19/09/26.
+//
