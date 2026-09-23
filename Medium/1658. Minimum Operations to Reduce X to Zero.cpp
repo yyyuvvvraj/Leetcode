631811ca5d4da546b26af853a66e713c66bcc54b
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-09-23 22:20:33 +0530

    1658

--- Medium/1658. Minimum Operations to Reduce X to Zero.cpp
+++ Medium/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -24,38 +24,6 @@ public:
         return longest==INT_MIN?-1:n-longest;
     }
 };
-
-//Approach-2 (Using Recursion) - You Can memoize it as well but it will still give TLE
-//T.C : O(2^n) -  We take and skip each ith and jth index
-//S.C : O(1) excluding Stack space
-class Solution {
-    int minCount;
-public:
-    void solve(vector<int>& nums, int remain, int count, int i, int j) {
-        if (remain == 0) {
-            minCount = min(minCount, count);
-            return;
-        }
-        else if (remain < 0 || i > j || count >= minCount)
-            return;
-        else {
-            solve(nums, remain - nums[i], count + 1, i + 1, j);
-            solve(nums, remain - nums[j], count + 1, i, j - 1);
-        }
-    }
-
-    int minOperations(vector<int>& nums, int x) {
-        int n = nums.size();
-        minCount = INT_MAX;
-
-        solve(nums, x, 0, 0, n-1);
-
-        if (minCount == INT_MAX)
-            return -1;
-
-        return minCount;
-    }
-};
 //
 // Created by Yuvraj Rajni Sachin Deshmukh on 23/09/26.
 //
