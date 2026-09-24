9bb5f73e0ff251b5abee36bd7c9b5746c44a6fba
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-09-24 21:25:56 +0530

    3550

---
+++ Easy/3550. Smallest Index With Digit Sum Equal to Index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i){
+            int total = 0;
+            for(int v = nums[i]; v > 0; v /= 10) total += v % 10;
+            if(total == i) return i;
+        }
+        return -1;
+    }
+};
+//
+// Created by Yuvraj Rajni Sachin Deshmukh on 24/09/26.
+//
